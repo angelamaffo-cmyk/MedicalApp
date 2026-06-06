cb12317b285f9c99da59d98cc5ba910f7ed3ace3
angelamaffo-cmyk <angela.maffo@saintjeaningenieur.org>
2026-06-06 17:13:20 +0100

    ajout creation automatique superuser

--- backend/build.sh
+++ backend/build.sh
@@ -11,4 +11,12 @@ python manage.py collectstatic --no-input
 # Appliquer les migrations de la base de données
 python manage.py migrate
 
-
+# Créer superuser automatiquement si il n'existe pas
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@medtrack.com', 'MedTrack2026')
+    print("Superuser créé!")
+else:
+    print("Superuser existe déjà.")
+"
