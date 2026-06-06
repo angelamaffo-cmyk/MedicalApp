8770a4fd1847d90d9febc8666bc3b678fb142d5d
angelamaffo-cmyk <angela.maffo@saintjeaningenieur.org>
2026-06-06 17:41:14 +0100

    ajout d'une page d'accueil pour l'api

--- backend/medtrack_api/urls.py
+++ backend/medtrack_api/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.http import JsonResponse
 
-
+def home_api(request):
+    
+    return JsonResponse({"status": "success", "message": "Bienvenue sur l'API MedTrack. Le backend fonctionne parfaitement !"})
 urlpatterns = [
+     path('', home_api, name='home'),
     path('admin/', admin.site.urls),
         path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
